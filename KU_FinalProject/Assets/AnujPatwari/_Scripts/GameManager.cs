b40d5e11a3088a30a979dd1c1a6ec4e9b020cd55
AnujPatwari <33886859+anuj-patwari@users.noreply.github.com>
2019-05-21 18:04:16 +0530

    Game timer working

--- KU_FinalProject/Assets/AnujPatwari/_Scripts/GameManager.cs
+++ KU_FinalProject/Assets/AnujPatwari/_Scripts/GameManager.cs
@@ -1,11 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
 
     [SerializeField] GameObject fruit;
+
+    //Game Timer
+    public Text gameTimerText;
+    float gameTimer = 0f;
 
     // Start is called before the first frame update
     void Start()
@@ -20,10 +25,19 @@ public class GameManager : MonoBehaviour
         {
             Instantiate(fruit, new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + 10, gameObject.transform.position.z), gameObject.transform.rotation);
         }
-    }
 
-    private void OnMouseDown()
-    {
-        
+        #region Game Timer
+        gameTimer += Time.deltaTime * 100;
+
+        //int seconds = (int)(gameTimer % 60);
+        int minutes = (int)(gameTimer / 60) % 60;
+        int hours = (int)(gameTimer / 3600) % 24;
+        int days = (int)(gameTimer / 86400) % 365;
+
+        string timerString = string.Format("Day {0:00} Time: {1:00}:{2:00}", days, hours, minutes);
+
+        gameTimerText.text = timerString;
+
+        #endregion
     }
 }
