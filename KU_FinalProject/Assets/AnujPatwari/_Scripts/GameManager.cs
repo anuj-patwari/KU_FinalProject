c447d985449b81f3ed31feb06b865be4569e8402
AnujPatwari <33886859+anuj-patwari@users.noreply.github.com>
2019-05-21 18:59:55 +0530

    Fruit instantiating done

--- KU_FinalProject/Assets/AnujPatwari/_Scripts/GameManager.cs
+++ KU_FinalProject/Assets/AnujPatwari/_Scripts/GameManager.cs
@@ -7,15 +7,19 @@ public class GameManager : MonoBehaviour
 {
 
     [SerializeField] GameObject fruit;
+    [SerializeField] int everyHowManyDays;
+    float counter;
 
     //Game Timer
     public Text gameTimerText;
-    float gameTimer = 0f;
+    public float gameTimer = 0f;
+    public int minutes, hours, days;
+    TreeScript ts;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        ts = FindObjectOfType<TreeScript>();
     }
 
     // Update is called once per frame
@@ -23,16 +27,16 @@ public class GameManager : MonoBehaviour
     {
         if (Input.GetKeyDown("space"))
         {
-            Instantiate(fruit, new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + 10, gameObject.transform.position.z), gameObject.transform.rotation);
+
         }
 
         #region Game Timer
-        gameTimer += Time.deltaTime * 100;
+        gameTimer += Time.deltaTime * 10000;
 
         //int seconds = (int)(gameTimer % 60);
-        int minutes = (int)(gameTimer / 60) % 60;
-        int hours = (int)(gameTimer / 3600) % 24;
-        int days = (int)(gameTimer / 86400) % 365;
+        minutes = (int)(gameTimer / 60) % 60;
+        hours = (int)(gameTimer / 3600) % 24;
+        days = (int)(gameTimer / 86400) % 365;
 
         string timerString = string.Format("Day {0:00} Time: {1:00}:{2:00}", days, hours, minutes);
 
--- KU_FinalProject/Assets/AnujPatwari/_Scripts/TreeScript.cs
+++ KU_FinalProject/Assets/AnujPatwari/_Scripts/TreeScript.cs
@@ -4,15 +4,63 @@ using UnityEngine;
 
 public class TreeScript : MonoBehaviour
 {
+    GameManager gm;
+    [SerializeField] GameObject fruit;
+    public int everyHowManyDays = 3;
+    float counter = 10;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        gm = FindObjectOfType<GameManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (gm.days % everyHowManyDays == 0 && gm.days != 0)
+        {
+            counter++;
+
+            if (counter < 3000)
+            {
+
+            }
+            if (counter == 2750)
+            {
+                Happen();
+            }
+            if (counter == 2800)
+            {
+                Happen();
+            }
+            if (counter == 2850)
+            {
+                Happen();
+            }
+            if (counter == 2900)
+            {
+                Happen();
+            }
+            if (counter == 2950)
+            {
+                Happen();
+            }
+            if (counter == 3000)
+            {
+                Happen();
+            }
+
+        }
+
+        else if (gm.days %everyHowManyDays != 0)
+        {
+            counter = 0;
+        }
+    }
+
+    public void Happen()
+    {
+        Instantiate(fruit, new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + 10, gameObject.transform.position.z), gameObject.transform.rotation);
     }
 }
